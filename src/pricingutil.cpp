4dd6bab0ff833d1adcab2e10732fb866259c0d16
WendyHuang26 <wenjinghuang26@gmail.com>
2023-10-12 19:39:52 -0500

    test

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,15 @@
 #include "pricingutil.h"
+
+
+PricingUtil::PricingUtil() {
+   
+}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
+    val = (prevPrice * (0.9f + interest)) * oleoConstant;
+    return val;
+}
+
+float PricingUtil::getVal() {
+    return val;
+}
--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,39 @@
+
 #include <gtest/gtest.h>
+#include "pricingutil.h"
+
 
 TEST(sampleTest, sample) {
     EXPECT_EQ(4, 4);
+}
+
+TEST(PricingUtilTest, normalCalc) {
+    PricingUtil util;
+    float result = util.calcVal(100.0f, 0.05f, 2.0f);
+    EXPECT_NEAR(result, 190.0f, 1e-6); // 100 * 0.95 * 2
+}
+
+TEST(PricingUtilTest, zeroInterest) {
+    PricingUtil util;
+    float result = util.calcVal(100.0f, 0.0f, 2.0f);
+    EXPECT_NEAR(result, 180.0f, 1e-6);  // 100 * 0.9 * 2 = 180
+}
+
+TEST(PricingUtilTest, negPrice) {
+    PricingUtil util;
+    float result = util.calcVal(-100.0f, 0.05f, 2.0f);
+    EXPECT_NEAR(result, -190.0f, 1e-6);  // -100 * 0.95 * 2 = -190
+}
+
+TEST(PricingUtilTest, bigOleo) {
+    PricingUtil util;
+    float result = util.calcVal(100.0f, 0.05f, 1e6f);
+    EXPECT_NEAR(result, 95000000.0f, 1e-6);  // 100 * 0.95 * 1e6 = 95000000
+}
+
+TEST(PricingUtilTest, getValTest) {
+    PricingUtil util;
+    util.calcVal(100.0f, 0.05f, 2.0f);
+    float value = util.getVal();
+    EXPECT_NEAR(value, 190.0f, 1e-6);
 }
