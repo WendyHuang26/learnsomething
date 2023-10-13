6a801743f23e68ecd6332c7927a2d31b431994d5
WendyHuang26 <wenjinghuang26@gmail.com>
2023-10-12 19:45:06 -0500

    test

--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -19,10 +19,28 @@ TEST(PricingUtilTest, zeroInterest) {
     EXPECT_NEAR(result, 180.0f, 1e-6);  // 100 * 0.9 * 2 = 180
 }
 
+TEST(PricingUtilTest, zeroPrevPrice) {
+    PricingUtil util;
+    float result = util.calcVal(0.0f, 0.05f, 2.0f);
+    EXPECT_NEAR(result, 0.0f, 1e-6);
+}
+
+TEST(PricingUtilTest, negativeInterest) {
+    PricingUtil util;
+    float result = util.calcVal(100.0f, -0.05f, 2.0f);
+    EXPECT_NEAR(result, 170.0f, 1e-6);  // 100 * 0.85 * 2 = 170
+}
+
 TEST(PricingUtilTest, negPrice) {
     PricingUtil util;
     float result = util.calcVal(-100.0f, 0.05f, 2.0f);
     EXPECT_NEAR(result, -190.0f, 1e-6);  // -100 * 0.95 * 2 = -190
+}
+
+TEST(PricingUtilTest, zeroOleo) {
+    PricingUtil util;
+    float result = util.calcVal(100.0f, 0.05f, 0.0f);
+    EXPECT_NEAR(result, 0.0f, 1e-6);
 }
 
 TEST(PricingUtilTest, bigOleo) {
